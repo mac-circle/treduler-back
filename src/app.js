c1e4e2f82ee452e28ecc982c73e402b0aaab4360
rontend <>
2019-03-20 18:19:22 +0900

    add middlewares bodyParser

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import API from "./apis";
 import helmet from "helmet";
 import cors from "cors";
 import ROUTES from "./routes/routes";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -12,6 +13,8 @@ const app = express();
 
 app.use(helmet());
 app.use(morgan("dev"));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(cors());
 app.use(ROUTES.apiRoute, API);
 
