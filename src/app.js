2a2c1d69188ac5025b387d19cae3fe877fc5e0c3
rontend <>
2019-03-20 15:24:10 +0900

    Install morgan and set morgan as node middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { HelloWorld } from "./middlewares/middlewares";
+import morgan from "morgan";
 import API from "./apis";
 
 const app = express();
 
-app.get("/", HelloWorld);
+// middlewares
+
+app.use(morgan("dev"));
 app.use("/api", API);
+
+// default showing part
+app.get("/", HelloWorld);
 
 export default app;
