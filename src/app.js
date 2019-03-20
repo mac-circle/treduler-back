0b80ee5e5d4a65a1e314bc771900cc297052d2ea
rontend <>
2019-03-20 15:27:26 +0900

    Install npm helmet and set it as node middleware

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import express from "express";
 import { HelloWorld } from "./middlewares/middlewares";
 import morgan from "morgan";
 import API from "./apis";
+import helmet from "helmet";
 
 const app = express();
 
 // middlewares
 
+app.use(helmet());
 app.use(morgan("dev"));
 app.use("/api", API);
 
