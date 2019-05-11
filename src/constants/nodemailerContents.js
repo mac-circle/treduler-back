25620cecfebbe2344e4a2ccbb3aab04cba591d3f
rontend <>
2019-05-11 19:28:50 +0900

    node mail html

---
+++ src/constants/nodemailerContents.js
@@ -0,0 +1,56 @@
+export const title =
+  "Hi, we are a treduler! Please verify your email account. ";
+export const text = "Please click the below url";
+export const html = `<div
+      style="
+      text-align: center;
+    "
+    >
+      <span
+        style="
+        color: black;
+        font-size: 38px;
+      "
+      >
+        Verify Your Email
+      </span>
+      <br />
+      <span
+        style="color: black;
+        
+      "
+        >Check your email & click the link to activate your account</span
+      >
+      <br />
+      <img
+        style="width: 200px;
+          height: 200px;
+          
+          "
+        src="https://cdn.windowsreport.com/wp-content/uploads/2018/09/download-Windows-Live-Mail.png"
+        alt="treduler-mail"
+      />
+      <br />
+      <a
+        href="http://treduler.com/token_value"
+        target="_blank"
+        rel="noopener noreferrer"
+        style="text-decoration: none !important;"
+      >
+        <button
+          style="width: 120px;
+          height: 40px;
+          text-align: center;
+          color: white;
+          border: 0;
+          background-color: #3498db;
+          font-size: 15px;
+          border-radius: 20px;
+          cursor: pointer;
+          
+          "
+        >
+          Verify Email
+        </button>
+      </a>
+    </div>`;
--- src/controllers/verify/request_verify.js
+++ src/controllers/verify/request_verify.js
@@ -1,17 +1,14 @@
 import User from "../../models/user";
 import { verifyingJWT } from "../../utils/generatingJWT";
 import { mailOptions, transporter } from "../../nodemailer";
+import { title, text, html } from "../../constants/nodemailerContents";
 
 const requestVerify = async (req, res, next) => {
   const token = req.headers.token;
   const id = verifyingJWT(token);
   try {
     const user = await User.findByPk(id);
-    const mailOption = mailOptions(
-      user.email,
-      "Hi, we are a treduler! Please verify your email account. ",
-      "Please click the below url"
-    );
+    const mailOption = mailOptions(user.email, title, text, html);
     transporter.sendMail(mailOption, (error, info) => {
       if (error) {
         console.log(error);
