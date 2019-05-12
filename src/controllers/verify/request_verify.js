c44d26f9d007cd441fc6d885e4426b0da15e8e54
rontend <>
2019-05-12 18:58:41 +0900

    email verify get request

--- src/controllers/verify/request_verify.js
+++ src/controllers/verify/request_verify.js
@@ -1,14 +1,21 @@
 import User from "../../models/user";
 import { verifyingJWT } from "../../utils/generatingJWT";
 import { mailOptions, transporter } from "../../nodemailer";
-import { title, text, html } from "../../constants/nodemailerContents";
+import { title, text, html, htmlFN } from "../../constants/nodemailerContents";
+import { hashingPassword } from "../../utils/hasingPassword";
 
 const requestVerify = async (req, res, next) => {
   const token = req.headers.token;
   const id = verifyingJWT(token);
   try {
     const user = await User.findByPk(id);
-    const mailOption = mailOptions(user.email, title, text, html);
+    const hashedUserEmailToken = await hashingPassword(user.email);
+    const mailOption = mailOptions(
+      user.email,
+      title,
+      text,
+      htmlFN(hashedUserEmailToken)
+    );
     transporter.sendMail(mailOption, (error, info) => {
       if (error) {
         console.log(error);
